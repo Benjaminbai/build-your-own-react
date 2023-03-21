022a6745f5825445f5ec7b38085677074aceb01c
benjamin <345831755@qq.com>
2023-03-21 11:30:58 +0800

    something went wrong of index3.js that will be retry in head

--- index3.js
+++ index3.js
@@ -30,8 +30,41 @@ function commitRoot() {
     wipRoot = null
 }
 
+const isEvent = key => key.startsWith('on')
+const isProperty = key => key !== 'children' && !isEvent(key)
+const isNew = (prev, next) => key => prev[key] !== next[key]
+const isGone = (prev, next) => key => !(key in next)
+
 function updateDom(dom, prevProps, nextProps) {
-    
+    //Remove old or changed event listeners
+    Object.keys(prevProps)
+        .filter(isEvent)
+        .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+        .forEach(name => {
+            const eventType = name.toLowerCase().substring(2)
+            dom.removeEventListener(eventType, prevProps[name])
+        })
+
+    // Add event listeners
+    Object.keys(nextProps)
+        .filter(isEvent)
+        .filter(isNew(prevProps, nextProps))
+        .forEach(name => {
+            const eventType = name.toLowerCase().substring(2)
+            dom.addEventListener(eventType, nextProps[name])
+        })
+
+    // Remove old properties
+    Object.keys(prevProps)
+        .filter(isProperty)
+        .filter(isGone(prevProps, nextProps))
+        .forEach(name => dom[name] = '')
+
+    // Set new or changed properties
+    Object.keys(nextProps)
+        .filter(isProperty)
+        .filter(isNew(prevProps, nextProps))
+        .forEach(name => dom[name] = nextProps[name])
 }
 
 function commitWork(fiber) {
@@ -40,11 +73,11 @@ function commitWork(fiber) {
     }
     const domParent = fiber.parent.dom
 
-    if(fiber.effectTag === 'PLACEMENT' && fiber.dom != null) {
+    if (fiber.effectTag === 'PLACEMENT' && fiber.dom != null) {
         domParent.appendChild(fiber.dom)
-    }else if(fiber.effectTag === 'DELETION') {
+    } else if (fiber.effectTag === 'DELETION') {
         domParent.removeChild(fiber.dom)
-    }else if(fiber.effectTag === 'UPDATE' && fiber.dom != null) {
+    } else if (fiber.effectTag === 'UPDATE' && fiber.dom != null) {
         updateDom(fiber.dom, fiber.alternate.props, fiber.props)
     }
     commitWork(fiber.child)
@@ -124,6 +157,15 @@ function reconcileChildren(wipFiber, elememnts) {
         if (oldFiber) {
             oldFiber = oldFiber.sibling
         }
+
+        if (index === 0) {
+            wipFiber.child = newFiber
+        } else if (element) {
+            prevSibling.sibling = newFiber
+        }
+
+        prevSibling = newFiber
+        index++
     }
 }
 
@@ -157,10 +199,7 @@ function createDom(fiber) {
         :
         document.createElement(fiber.type)
 
-    const isProperty = key => key !== 'children'
-    Object.keys(fiber.props).filter(isProperty).forEach(name => {
-        dom[name] = fiber.props[name]
-    })
+    updateDom(dom, {}, fiber.props)
     return dom
 }
 
